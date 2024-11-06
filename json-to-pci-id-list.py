55dfb368bf0c87c5fa3044db5474aa5bcfdca5df
Simone Caronni <scaronni@nvidia.com>
2024-11-06 15:36:47 +0100

    Simplify pci_id generation

--- json-to-pci-id-list.py
+++ json-to-pci-id-list.py
@@ -1,49 +1,35 @@
-#!/usr/bin/env python3
- # Copyright (c) 2022 Tamara Schmitz <tamara.schmitz@suse.com>.
- # 
- # This program is free software: you can redistribute it and/or modify  
- # it under the terms of the GNU General Public License as published by  
- # the Free Software Foundation, version 3.
- #
- # This program is distributed in the hope that it will be useful, but 
- # WITHOUT ANY WARRANTY; without even the implied warranty of 
- # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
- # General Public License for more details.
- #
- # You should have received a copy of the GNU General Public License 
- # along with this program. If not, see <http://www.gnu.org/licenses/>.
- #
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2024 Simone Caronni <negativo17@gmail.com>
+# Licensed under the GNU General Public License Version or later
 
 import argparse
 import json
 
-# arg parsing
-parser = argparse.ArgumentParser(description="Parse an NVIDIA supported devices\
-        JSON and convert it to a pci id list.")
-parser.add_argument("INPUT_JSON",
-        help="The JSON file to be parsed",
-        type=argparse.FileType('r')
-        )
-parser.add_argument("--skiplegacy", help="Skip GPUs that are in a legacy branch",
-                    action="store_true")
-parser.add_argument("--kernelopen", help="Only select GPUs that are supported by\
-        the Open GPU kernel modules",
-                    action="store_true")
+parser = argparse.ArgumentParser(description="Parse a supported-gpus.json file and prints a RPM supplement list.")
+parser.add_argument("--flavor", help="Specify kernel flavor", required=True)
+parser.add_argument("INPUT_JSON", help="The JSON file to be parsed", type=argparse.FileType('r'))
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("--closed", help="Select GPUs supported only by the closed modules", action="store_true")
+group.add_argument("--open", help="Select GPUs supported by the open module", action="store_true")
 args = parser.parse_args()
 
-# json parsing
-json = json.load(args.INPUT_JSON)
-pci_id_list = {}
-for chip in json["chips"]:
-    if args.skiplegacy and "legacybranch" in chip:
-        continue
-    if args.kernelopen and \
-            ("features" not in chip or "kernelopen" not in chip["features"]):
-        continue
-    if "devid" in chip and "name" in chip:
-        pci_id_list[chip["devid"]] = chip["name"]
+gpus_raw = json.load(args.INPUT_JSON)
 
-# write to file
-for devid, name in sorted(pci_id_list.items(), key=lambda i: i[0]):
-    # there are no duplicates since a dictionary's key is unique
-    print(f"{devid} {name}")
+devids = []
+
+for product in gpus_raw["chips"]:
+
+    if args.closed and "legacybranch" not in product.keys() and "kernelopen" not in product["features"]:
+        continue
+
+    if args.open and "kernelopen" in product["features"]:
+        continue
+
+    gpu = product["devid"].replace("0x",'')
+    if not gpu in devids:
+        devids.append(gpu)
+
+for gpu in devids:
+    print(f"Supplements modalias(kernel-" + args.flavor + ":pci:v000010DEd0000" + gpu + "sv*sd*bc03sc0[02]i00*)")
